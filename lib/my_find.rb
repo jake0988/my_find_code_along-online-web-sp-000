c04134bf85f836e2e0af14c074ff64b050f9db7b
Jake G <jake098877@gmail.com>
2020-04-29 02:07:41 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -11,4 +11,5 @@ def my_find(collection)
     i = i + 1
   end
 end
-  my_find(collection) {|blah| 1 % 3 == 0 and 1 % 5 == 0 }
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
